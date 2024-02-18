a28b3212f1b6036ab84e1587574f4d51967e2e31
gunyu1019 <gunyu1019@gmail.com>
2024-02-19 01:13:30 +0900

    [Feat] #2 - Add `_response_pydantic_model_validation` decorator to validate model from json

--- chzzkpy/http.py
+++ chzzkpy/http.py
@@ -1,18 +1,49 @@
+import functools
+import inspect
+
+import aiohttp
 from async_client_decorator import Session, get, Path
 from typing import Annotated
 
-from .live_status import LiveStatus
+from .base_model import ChzzkModel, Content
+from .live_status import LiveStatus, LiveDetail
+
+
+def _response_pydantic_model_validation(func):
+    signature = inspect.signature(func)
+    if not issubclass(signature.return_annotation, ChzzkModel):
+        return func
+
+    signature = inspect.signature(func)
+
+    @functools.wraps(func)
+    async def wrapper(self, response: aiohttp.ClientResponse, *_1, **_2):
+        data = await response.json()
+        validated_data = signature.return_annotation.model_validate(data)
+        return validated_data
+    return wrapper
+
+
+def _response_pydantic_model_validation_able(func):
+    signature = inspect.signature(func)
+    if not issubclass(signature.return_annotation, ChzzkModel):
+        return func
+
+    func.__component_parameter__.response.append("response")
+    return func
 
 
 class ChzzkAPISession(Session):
     def __init__(self):
         super().__init__(base_url="https://api.chzzk.naver.com")
 
-    @get("/polling/v2/channels/{channel_id}/")
+    @_response_pydantic_model_validation_able
+    @get("/polling/v2/channels/{channel_id}/live-status")
+    @_response_pydantic_model_validation
     async def live_status(
             self,
             channel_id: Annotated[str, Path]
-    ) -> LiveStatus:
+    ) -> Content[LiveStatus]:
         pass
 
 
