04123e50e5aa0562141f65652b93fde9191fa7a6
gunyu1019 <gunyu1019@gmail.com>
2024-02-19 01:12:42 +0900

    [Feat] #1 - Add Content Model

--- chzzkpy/base_model.py
+++ chzzkpy/base_model.py
@@ -1,8 +1,18 @@
+from typing import Generic, TypeVar
+
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
+
+T = TypeVar('T')
 
 
 class ChzzkModel(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_camel,
     )
+
+
+class Content(ChzzkModel, Generic[T]):
+    code: int
+    message: str | None
+    content: T
