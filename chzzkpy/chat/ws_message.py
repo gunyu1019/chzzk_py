c5a5fa3ded063ca15d60b48277050be45c49bf38
gunyu1019 <gunyu1019@gmail.com>
2024-02-25 02:11:45 +0900

    [Feat] Change field name (svcid = serviceId)

--- chzzkpy/chat/ws_message.py
+++ chzzkpy/chat/ws_message.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import Field
 
 from .enums import ChatCmd
@@ -5,10 +6,10 @@ from ..base_model import ChzzkModel
 
 
 class WSMessageBase(ChzzkModel):
-    svcid: str = 'game'
+    service_id: str = Field('game', alias='svcid')
 
     channel_id: str = Field(alias='cid')
-    version: str = Field('ver')
-    tid: str
+    version: str = Field(alias='ver')
+    tid: Optional[str]  # IDK: Full Name
 
     cmd: ChatCmd
