2cc33bddea9ada36f679f375a98888cbfceabec2
gunyu1019 <gunyu1019@gmail.com>
2024-02-25 02:34:01 +0900

    [Feat] Delete WSMessage (without modeling)

--- chzzkpy/chat/connected.py
+++ chzzkpy/chat/connected.py
@@ -1,8 +1,7 @@
 from typing import Literal, Optional
+
 from pydantic import Field
 
-from .enums import ChatCmd
-from .ws_message import WSMessageBase
 from ..base_model import ChzzkModel
 
 
@@ -11,10 +10,3 @@ class ConnectedInfo(ChzzkModel):
     auth: Literal['SEND', 'READ']
     uuid: Optional[str]
     session_id: str = Field(alias='sid')
-
-
-class WSMessageConnected(WSMessageBase):
-    cmd: ChatCmd = ChatCmd.CONNECTED
-    ret_code: int
-    ret_message: str = Field(alias='ret_msg')
-    body: ConnectedInfo
--- chzzkpy/chat/recent_chat.py
+++ chzzkpy/chat/recent_chat.py
@@ -1,9 +1,6 @@
-from typing import Literal, Optional
-from pydantic import Field
+from typing import Optional
 
-from .enums import ChatCmd
 from .message import ChatMessage, NoticeMessage
-from .ws_message import WSMessageBase
 from ..base_model import ChzzkModel
 
 
@@ -11,10 +8,3 @@ class RecentChat(ChzzkModel):
     message_list: list[ChatMessage]
     user_count: Optional[int]
     notice: Optional[NoticeMessage]
-
-
-class WSRecentChat(WSMessageBase):
-    cmd: ChatCmd = ChatCmd.RECENT_CHAT
-    ret_code: int
-    ret_message: str = Field(alias='ret_msg')
-    body: RecentChat = Field(alias='bdy')
