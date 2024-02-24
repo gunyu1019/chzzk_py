5a0c172851ad6703f94e423001d8432fd064c8bf
gunyu1019 <gunyu1019@gmail.com>
2024-02-25 04:07:25 +0900

    [Feat] Add Blind data model

---
+++ chzzkpy/chat/blind.py
@@ -0,0 +1,14 @@
+import datetime
+from typing import Optional
+from pydantic import Field
+
+from ..base_model import ChzzkModel
+
+
+class Blind(ChzzkModel):
+    service_id: str
+    time: datetime.datetime = Field(alias='messageTime')
+    blind_type: str
+    blind_user_id: Optional[str]
+    user_id: str
+    message: Optional[str]
