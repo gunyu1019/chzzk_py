a423e85ed12b4b831b32205253e79a24a4087a18
gunyu1019 <gunyu1019@gmail.com>
2024-02-26 16:12:03 +0900

    [Feat] Add chatting_tts example (WIP)

---
+++ examples/chatting_tts.py
@@ -0,0 +1,28 @@
+import io
+import gtts
+import pydub
+import pydub.playback
+from chzzkpy.chat import ChatClient, ChatMessage
+
+
+# Configuration
+channel_id = "21a85ed3ac126f622a05cd670c1be535"
+
+
+client = ChatClient(channel_id)
+
+
+@client.event
+async def on_connect():
+    print("Ready bot.")
+
+
+@client.event
+async def on_chat(message: ChatMessage):
+    tts = gtts.gTTS(text=message.content, lang='ko', slow=False)
+    fp = io.BytesIO()
+    tts.write_to_fp(fp)
+    # WIP
+
+
+client.run()
