f12da04a84a0f1ae98c26da081f5ded5b6010cd3
gunyu1019 <gunyu1019@gmail.com>
2024-11-10 05:23:40 +0000

    [Docs] #29 - Change sidebar title and elements.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('chat'))
+
 project = 'chzzkpy'
 copyright = '2024, gunyu1019'
 author = 'gunyu1019'
@@ -14,15 +21,32 @@ release = 'v1.0.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+add_module_names = False
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'ko'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'puro'
+html_theme = 'furo'
 html_static_path = ['_static']
+
+## Template Option
+html_title = "chzzkpy"
+html_sidebars = {
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+    ]
+}
